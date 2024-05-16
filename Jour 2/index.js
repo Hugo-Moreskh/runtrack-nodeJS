1c742527fa3f1991fdd32294891531c1f63d651a
Hugo-Moreskh <hugo.moreskh@laplateforme.io>
2024-05-16 18:00:58 +0200

    jour 02

--- Jour 2/index.js
+++ Jour 2/index.js
@@ -1,7 +1,7 @@
 const server = require("./server");
 
-const PORT = process.env.PORT || 8000;
+const PORT = process.env.PORT || 3000;
 
 server.listen(PORT, () => {
-  console.log(`Server is listening on port ${PORT}`);
+  console.log(`Server is running on port ${PORT}`);
 });
